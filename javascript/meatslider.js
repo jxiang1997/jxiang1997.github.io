c9da08e2a072343e076ddc857ada0b591fb1e350
Justin Xiang <jxiang@mit.edu>
2019-11-19 03:16:59 -0500

    updated

---
+++ javascript/meatslider.js
@@ -0,0 +1,13 @@
+/* JS logic for meat slides */
+
+
+
+$(document).ready(function() {
+    // Beef
+    var beef = $('#beef');
+    var beef_carbon = $('#beef_carbon');
+
+    // Poultry 
+    var poultry = $('#poultry');
+    var poultry_carbon = $('#poultry_carbon')
+})
